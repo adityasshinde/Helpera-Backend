66c69e7649a48084fb0e2bfd18886e164f692800
GauravTiwaritp <gauravtiwari282001@gmail.com>
2023-04-29 21:44:05 +0530

    required dependency installed and sensitive information are now hidden

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const router = require("./routes/post.js");
 const userRoute = require("./routes/userRoute.js");
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -11,12 +10,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/user", userRoute);
-const CONNECTION_URL =
-  "mongodb+srv://Gaurav:123123juvi@nodeproject1.8pzpuwr.mongodb.net/?retryWrites=true&w=majority";
 
 const port = process.env.PORT || 5000;
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(MONGO_URI)
   .then(() =>
     app.listen(port, () => console.log(`server is connected on ${port}`))
   )
