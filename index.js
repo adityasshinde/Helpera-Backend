e15c55858eea69825990fae42d67f0fce8c057b2
adityasshinde <s.adityashinde11@gmail.com>
2023-04-30 19:38:06 +0530

    .env issue resolved

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-require("dotenv").config;
+require("dotenv").config();
 const userRoute = require("./routes/userRoute");
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -17,5 +17,5 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() =>
     app.listen(port, () => console.log(`server is connected on ${port}`))
-  )
+  ) 
   .catch((error) => console.log(error.message));
